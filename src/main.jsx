4b74d6b67c2f1bff72f2dbfb3371195fb78e40b4
Segun <adoughdough@yahoo.com>
2024-03-01 11:36:30 +0100

    added comment

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+// this is a demo change
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
